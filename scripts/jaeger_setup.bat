6f17518b7396828e991dc0953dc266d6e59ae119
szaboegon <szabo.egon2001@gmail.com>
2024-09-22 16:39:25 +0200

    Deploy heartbeat app for testing

--- scripts/jaeger_setup.bat
+++ scripts/jaeger_setup.bat
@@ -66,7 +66,7 @@ echo Port forwarding Jaeger UI to localhost port 16686
 set MAX_RETRIES=10
 set RETRY_COUNT=0
 :RETRY_PORT_FORWARD
-kubectl port-forward -n observability  deployments/my-jaeger 16686
+kubectl port-forward -n observability deployments/jaeger 16686
 if errorlevel 1 (
     set /a RETRY_COUNT+=1
     if %RETRY_COUNT% lss %MAX_RETRIES% (
