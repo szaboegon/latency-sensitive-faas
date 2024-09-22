5fd0fc51bb804bee2e8b326ab6667eee150beec2
szaboegon <szabo.egon2001@gmail.com>
2024-09-22 10:50:13 +0200

    Add retry for port forward

--- scripts/jaeger_setup.bat
+++ scripts/jaeger_setup.bat
@@ -36,14 +36,14 @@ echo Deploying Jaeger instance into cluster...
 rem Retry if deploy fails, it means the operator is not initialized yet
 set MAX_RETRIES=10
 set RETRY_COUNT=0
-:RETRY
+:RETRY_JAEGER
 kubectl apply -f %JAEGER_INSTANCE_PATH% -n observability
 if errorlevel 1 (
     set /a RETRY_COUNT+=1
     if %RETRY_COUNT% lss %MAX_RETRIES% (
         echo Retry %RETRY_COUNT% of %MAX_RETRIES% failed. Retrying...
         timeout /t 10 >nul
-        goto RETRY
+        goto RETRY_JAEGER
     ) else (
         echo Failed to deploy Jaeger instance after %MAX_RETRIES% retries
         exit /b 1
@@ -63,4 +63,18 @@ if errorlevel 1 (
 )
 
 echo Port forwarding Jaeger UI to localhost port 16686
+set MAX_RETRIES=10
+set RETRY_COUNT=0
+:RETRY_PORT_FORWARD
 kubectl port-forward -n observability  deployments/my-jaeger 16686
+if errorlevel 1 (
+    set /a RETRY_COUNT+=1
+    if %RETRY_COUNT% lss %MAX_RETRIES% (
+        echo Retry %RETRY_COUNT% of %MAX_RETRIES% failed. Retrying...
+        timeout /t 10 >nul
+        goto RETRY_PORT_FORWARD
+    ) else (
+        echo Failed to forward port after %MAX_RETRIES% retries
+        exit /b 1
+    )
+)
