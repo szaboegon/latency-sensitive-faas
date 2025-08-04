af66be4849e10acbe78d6756e995b6d80b0a59e2
szaboegon <szabo.egon2001@gmail.com>
2025-08-04 13:05:11 +0200

    Add port forward for tekton dashboard

--- kubernetes/port_forward.ps1
+++ kubernetes/port_forward.ps1
@@ -15,6 +15,7 @@ $null = Register-EngineEvent -SourceIdentifier Console_CancelKeyPress -Action $c
 Start-Process -NoNewWindow -FilePath "kubectl" -ArgumentList "port-forward", "svc/kibana-kb-http", "5601:5601", "-n", "observability"
 Start-Process -NoNewWindow -FilePath "kubectl" -ArgumentList "port-forward", "svc/lsf-configurator", "8080:80", "-n", "configurator"
 Start-Process -NoNewWindow -FilePath "kubectl" -ArgumentList "port-forward", "svc/sqlite-web", "8085:8080", "-n", "configurator"
+Start-Process -NoNewWindow -FilePath "kubectl" -ArgumentList "port-forward", "svc/tekton-dashboard", "9097:9097", "-n", "tekton-pipelines"
 
 Write-Host "Port-forwards started. Press Ctrl+C to stop them."
 
