77c548f5bdb062a1eaab265228c973817b80acae
szaboegon <szabo.egon2001@gmail.com>
2025-08-11 14:53:21 +0200

    Fix style inconcsistencies

--- lsf-configurator/frontend/src/main.tsx
+++ lsf-configurator/frontend/src/main.tsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import CssBaseline from '@mui/material/CssBaseline';
 
 const queryClient = new QueryClient({});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
+      <CssBaseline />
         <App />
     </QueryClientProvider>
   </StrictMode>,
