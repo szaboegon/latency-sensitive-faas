784a5fd95a0dc3dc18bc8abcfc1f54c309172195
szaboegon <szabo.egon2001@gmail.com>
2025-10-25 15:43:06 +0200

    Add new benchmark

--- lsf-configurator/pkg/kubeclient/client.go
+++ lsf-configurator/pkg/kubeclient/client.go
@@ -2,9 +2,12 @@ package kubeclient
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 
 	istioclient "istio.io/client-go/pkg/clientset/versioned"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 type Client struct {
@@ -14,7 +17,16 @@ type Client struct {
 func NewKubeclient() (*Client, error) {
 	cfg, err := rest.InClusterConfig()
 	if err != nil {
-		return nil, fmt.Errorf("failed to load in-cluster config: %w", err)
+		// Try local kubeconfig for local debugging
+		home, homeErr := os.UserHomeDir()
+		if homeErr != nil {
+			return nil, fmt.Errorf("failed to load in-cluster config: %v, and failed to get home dir: %w", err, homeErr)
+		}
+		kubeconfig := filepath.Join(home, ".kube", "config")
+		cfg, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
+		if err != nil {
+			return nil, fmt.Errorf("failed to load in-cluster config: %v, and failed to load local kubeconfig: %w", err, err)
+		}
 	}
 
 	ic, err := istioclient.NewForConfig(cfg)
