daf12139d8c30d4626d5218b31f1609a73fb81a9
szaboegon <szabo.egon2001@gmail.com>
2025-11-07 20:15:06 +0100

    Add graph generation to eval

--- tools/jmeter/run_eval.ps1
+++ tools/jmeter/run_eval.ps1
@@ -1,5 +1,19 @@
 Remove-Item -Recurse -Force "dashboard-report" -ErrorAction SilentlyContinue
 Remove-Item -Force "jmeter.log" -ErrorAction SilentlyContinue
-Remove-Item -Force "results.csv" -ErrorAction SilentlyContinue
+Remove-Item -Force "results.jtl" -ErrorAction SilentlyContinue
+Remove-Item -Force "response_times.png" -ErrorAction SilentlyContinue
+Remove-Item -Force "transactions.png" -ErrorAction SilentlyContinue
 
-jmeter -n -t "eval.jmx" -l "results.csv" -e -o "dashboard-report"
+& "$env:JMETER_HOME\bin\jmeter.bat" -n -t "eval.jmx" -l "results.jtl" -e -o "dashboard-report"
+
+& "$env:JMETER_HOME\bin\JMeterPluginsCMD.bat" `
+ --generate-png "response_times.png" `
+ --input-jtl "results.jtl" `
+ --plugin-type "ResponseTimesOverTime" `
+ --width 1920 --height 1080
+
+& "$env:JMETER_HOME\bin\JMeterPluginsCMD.bat" `
+ --generate-png "transactions.png" `
+ --input-jtl "results.jtl" `
+ --plugin-type "TransactionsPerSecond" `
+ --width 1920 --height 1080 
