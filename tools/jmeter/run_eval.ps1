8233ff9b9e322b72ca2070b595a7eda1be266a3b
szaboegon <szabo.egon2001@gmail.com>
2025-11-07 07:37:20 +0100

    Run eval script modification

--- tools/jmeter/run_eval.ps1
+++ tools/jmeter/run_eval.ps1
@@ -1,1 +1,5 @@
+Remove-Item -Recurse -Force "dashboard-report" -ErrorAction SilentlyContinue
+Remove-Item -Force "jmeter.log" -ErrorAction SilentlyContinue
+Remove-Item -Force "results.csv" -ErrorAction SilentlyContinue
+
 jmeter -n -t "eval.jmx" -l "results.csv" -e -o "dashboard-report"
